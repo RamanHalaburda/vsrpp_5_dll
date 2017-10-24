554eff33ba6d35fcaba5fc05bf867760cd98d749
RomanHalaburda <mtbrider94@gmail.com>
2017-10-24 18:31:44 +0300

    dll done!

---
+++ StandartSounds/PlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Media;
+
+namespace StandartSounds
+{
+    public class PlaySound
+    {
+        public static void PlayAsterisk()
+        {
+            SystemSounds.Asterisk.Play();
+        }
+
+        public static void PlayBeep()
+        {
+            SystemSounds.Beep.Play();
+        }
+
+        public static void PlayExclamation()
+        {
+            SystemSounds.Exclamation.Play();
+        }
+
+        public static void PlayHand()
+        {
+            SystemSounds.Hand.Play();
+        }
+
+        public static void PlayQuestion()
+        {
+            SystemSounds.Question.Play();
+        }
+    }
+}
